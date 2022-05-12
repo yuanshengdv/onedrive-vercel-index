4531964e902876c7436f71f0608d6df25afd9c4a
yuanshengdv <distant_voices1996@outlook.com>
2022-05-12 18:29:16 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -11,10 +11,10 @@ module.exports = {
 
   // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
   // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
-  icon: '/icons/128.png',
+  icon: '/icons/128.jpg',
 
   // The name of your website. Present alongside your icon.
-  title: "Spencer's OneDrive",
+  title: "distant_voices's OneDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/',
@@ -50,7 +50,7 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      link: 'https://github.com/yuanshengdv/onedrive-vercel-index',
     },
     {
       name: 'Telegram',
